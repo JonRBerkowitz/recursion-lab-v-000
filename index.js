4927e4dfaad6707d2ba0c51412171fdde51bc929
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-05-28 20:38:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Code your solution here!
 
 function printString(string) {
-  console.log(string);
+  console.log(string[0]);
 
+  if (string.length > 1) {
+    let subStr = string.substring(1, string.length);
+    printString(subStr)
+  } else {
+    return true
+  }
 }
