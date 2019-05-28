609c059291cee6c6dcbbd7a643192bfde8a61574
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-05-28 21:25:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function reverseString(string) {
 function isPalindrome(str) {
 
   if (str.length > 1) {
-    if (str[0] === str[length - 1]) {
-      isPalindrome(str.substring(1, str.length - 1))
+    if (str[0] === str[str.length - 1]) {
+      return isPalindrome(str.substring(1, str.length - 1))
     } else {
       return false
     }
