1be3440d87f0ab50993ff6d111007d0d3305347e
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-05-28 20:57:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function printString(string) {
     return true
   }
 }
+
+function reverseString(string) {
+  
+  if (string.length > 1) {
+    return string[string.length - 1] + reverseString(string.substring(0,string.length - 1))
+    
+  } else {
+    return ""
+  }
+}
