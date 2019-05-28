2b69175583189e8240c3857b0c3e296aa24aaf9f
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-05-28 21:01:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ function printString(string) {
 
 function reverseString(string) {
 
-  if (string.length >= 1) {
-    return string[string.length - 1] + reverseString(string.substring(0,string.length - 1))
-
+  if (string.length > 0) {
+    return string[string.length - 1] + reverseString(string.substring(0, string.length - 1))
   } else {
-    return ""
+    return "";
   }
+
 }
